0bae6d44bbcefa77936935fe823fed1302bd77e4
Diego <diegogoleon@hotmail.com>
2024-11-12 17:43:13 -0600

    Corrigiendo errores de ector

--- app/src/main/java/com/example/gitapp/MainActivity.kt
+++ app/src/main/java/com/example/gitapp/MainActivity.kt
@@ -20,10 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             GitAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
